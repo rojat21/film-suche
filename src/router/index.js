a2e502a954b74281b2a9c68baf6e0e68873e8237
rojat21 <rojat720@gmail.com>
2026-04-29 10:11:34 +0200

    Router konfiguriert

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,16 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DetailView from '../views/DetailView.vue'
+import FavoritenView from '../views/FavoritenView.vue'
+import WatchlistView from '../views/WatchlistView.vue'
+import KategorienView from '../views/KategorienView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  { path: '/', component: HomeView },
+  { path: '/detail/:id', component: DetailView },
+  { path: '/favoriten', component: FavoritenView },
+  { path: '/watchlist', component: WatchlistView },
+  { path: '/kategorien', component: KategorienView }
 ]
 
 const router = createRouter({
